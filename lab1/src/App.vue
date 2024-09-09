b1cfb6128842ee1d81f1fbb55415cb1da47f36d5
Mariia Barnash <m.barnash@meine-landausfluege.de>
2024-09-09 19:04:47 +0300

    Vue.js lab 1

---
+++ lab1/src/App.vue
@@ -0,0 +1,8 @@
+<script setup>
+import Info from './components/Info.vue';
+</script>
+
+<template>
+  <Info name='Mariia Barnash' specialty='121 Software Engineering'
+        courseNumber='2' group='8.1213-іпз' />;
+</template>
---
+++ lab1/src/components/Info.vue
@@ -0,0 +1,30 @@
+
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  name: String,
+  specialty: String,
+  courseNumber: Number,
+  group: String,
+})
+
+const count = ref(0)
+</script>
+
+<template>
+  <div class="info">
+    <h1>{{ name }}</h1>
+    <p>{{ specialty }}</p>
+    <p>{{ courseNumber }} course</p>
+    <p>{{ group }}</p>
+    <p>Count: {{ count }}</p>
+    <button @click="count++">Increment</button>
+  </div>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
